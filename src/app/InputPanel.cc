bc789093d4b79939bd1d7116508e0e20d39335a6
dsxxm <2531222159@qq.com>
2026-07-30 11:50:50 +0800

    fix mutiple line and ui

--- src/app/InputPanel.cc
+++ src/app/InputPanel.cc
@@ -3,6 +3,13 @@
 // public
 
 InputPanel::InputPanel() {
+  // config
+  set_policy(Gtk::PolicyType::NEVER, Gtk::PolicyType::AUTOMATIC);
+  text_view.set_wrap_mode(Gtk::WrapMode::WORD_CHAR);
+  Glib::RefPtr<Gtk::CssProvider> provider = Gtk::CssProvider::create();
+  provider->load_from_data("#input_textview{font:22px monospace;}");
+  text_view.set_name("input_textview");
+  text_view.get_style_context()->add_provider(provider, 1);
   // add something
   set_child(text_view);
   buffer = text_view.get_buffer();
--- src/app/OutputPanel.cc
+++ src/app/OutputPanel.cc
@@ -1,9 +1,19 @@
 #include "app/OutputPanel.h"
+#include <glibmm/refptr.h>
+#include <gtkmm/cssprovider.h>
 
 OutputPanel::OutputPanel() {
+  // config
+  set_policy(Gtk::PolicyType::NEVER, Gtk::PolicyType::AUTOMATIC);
+  text_view.set_wrap_mode(Gtk::WrapMode::WORD_CHAR);
+  text_view.set_editable(false);
+  Glib::RefPtr<Gtk::CssProvider> provider = Gtk::CssProvider::create();
+  provider->load_from_data("#output_textview{font:22px monospace;}");
+  text_view.set_name("output_textview");
+  text_view.get_style_context()->add_provider(provider, 1);
+  // add somthing
   set_child(text_view);
 
-  text_view.set_editable(false);
   buffer = text_view.get_buffer();
   buffer->set_text("output here");
 }
